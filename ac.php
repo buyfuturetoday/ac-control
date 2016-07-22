0095625c5577078ceaf74c327eab12bbb1cd5fea
Skylar Gasai <skylar@yandere.love>
2016-07-22 00:40:21 -0500

    Preliminary heat support + set point over HTTP

--- ac.php
+++ ac.php
@@ -1,30 +1,76 @@
+<?php
+if(isset($_POST['on'])) {
+	shell_exec('sudo python on.py');
+	echo "Forcing AC on\n";
+}
+if(isset($_POST['off'])) {
+	shell_exec('sudo python off.py');
+	echo "Allowing AC off\n";
+}
+if(isset($_POST['open'])) {
+	shell_exec('sudo python open.py');
+	echo "Opened air vent\n";
+}
+if(isset($_POST['close'])) {
+	shell_exec('sudo python close.py');
+	echo "Closed air vent\n";
+}
+if(isset($_POST['run'])) {
+	shell_exec('sudo python run.py > /dev/null 2>&1 &');
+	echo "Running AC for 25 minutes...\n";
+}
+if(isset($_POST['setpoint'])) {
+	file_put_contents('setpoint', $_POST['set']);
+	echo "Changed set point to [" . $_POST['set'] . "]\n";
+}
+if(isset($_GET['on'])) {
+	shell_exec('sudo python on.py');
+	echo "Forcing AC on\n";
+}
+if(isset($_GET['off'])) {
+	shell_exec('sudo python off.py');
+	echo "Allowing AC off\n";
+}
+if(isset($_GET['run'])) {
+	shell_exec('sudo python run.py > /dev/null 2>&1 &');
+	echo "Running AC for 55 minutes...\n";
+}
+if(isset($_GET['open'])) {
+	shell_exec('sudo python open.py');
+	echo "Opened air vent\n";
+}
+if(isset($_GET['close'])) {
+	shell_exec('sudo python close.py');
+	echo "Closed air vent\n";
+}
+if(isset($_GET['set'])) {
+	file_put_contents('setpoint', $_POST['set']);
+	echo "Changed set point to [" . $_POST['set'] . "]\n";
+}
+?>
 <!DOCTYPE html>
 <html>
+<style>
+input {
+	border:1px solid black;
+}
+</style>
+<title>HVAC Controls</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <body style="max-width:400px; background-color:#333; color:#FFF;">
 <h3>
 <?php
-
 include("acstate.html");
-
 echo '</h3><form method="POST" action="ac.php">';
-echo '<input type="submit" name="on" value="Switch AC ON" style="padding:18px; margin:8px; background-color:#0F0;"><input type="submit" name="off" value="Switch AC OFF" style="padding:18px; margin:8px; background-color:#F00;"></form>';
-if(isset($_POST['on'])) {
-	shell_exec('sudo python on.py');
-	echo "Done\n";
-}
-if(isset($_POST['off'])) {
-	shell_exec('sudo python off.py');
-	echo "Done\n";
-}
-if(isset($_GET['on'])) {
-	shell_exec('sudo python on.py');
-	echo "Done\n";
-}
-if(isset($_GET['off'])) {
-	shell_exec('sudo python off.py');
-	echo "Done\n";
-}
+echo '<input type="submit" name="on" value="Switch AC ON" style="padding:18px; width:40%; margin:8px; background-color:#0F0;"><input type="submit" name="off" value="Switch AC OFF" style="padding:18px; width:40%; margin:8px; background-color:#F00;"><br><input type="submit" name="run" value="Run for 55m" style="padding:18px; width:80%; margin:8px; background-color:#777;">';
+echo '<h3>';
+include("ventstate.html");
+echo '</h3>';
+echo '<input type="submit" name="open" value="Open Vent" style="padding:18px; width:40%; margin:8px; background-color:#0F0;"><input type="submit" name="close" value="Close Vent" style="padding:18px; width:40%; margin:8px; background-color:#F00;">';
+echo '<h3>Set point</h3><input type="number" name="set" min="72" max="76" style="font-size:36px; width:40%; margin:8px;" value=';
+echo (file_get_contents ("setpoint"));
+echo '><input type="submit" name="setpoint" value="Set" style="padding:18px; width:40%; margin:8px; background-color:#777;"></form>';
+
 ?>
 </body>
 </html>
--- tstat.pl
+++ tstat.pl
@@ -1,9 +1,22 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+
+# Get set point
 my @sp = qx/cat setpoint/;
 chomp($sp[0]);
 print "Set point is " . $sp[0] . "F\n";
+
+# Get mode
+my @mode = qx/cat mode/;
+chomp($mode[0]);
+if ($mode[0] =~ /cool/) {
+	print "Cooling mode: run air conditioner\n";
+} elsif ($mode[0] =~ /heat/) {
+	print "Heating mode: run furnace\n";
+}
+
+# Check current temperature & humidity
 my @output = qx/python room.py/;
 my $f;
 my $c;
@@ -20,6 +33,8 @@ foreach (@output) {
 	}
 }
 print "Room is at " . $f . "F (" . $c . "C) with " . $h . "% humidity\n";
+
+# Check vent state
 my @ventstate = qx/cat ventstate.html/;
 if ($ventstate[0] =~ /OPEN/) {
 	print "Air vent is open\n";
@@ -28,26 +43,51 @@ if ($ventstate[0] =~ /OPEN/) {
 } else {
 	die "Can't get vent state\n";
 }
-if ($f < ($sp[0] - 1)) {
-	print "1 degree (or more) below set point, too cold\n";
-	if ($ventstate[0] =~ /CLOSED/) {
-		print "Air vent already closed, nothing to do\n";
-	} elsif ($ventstate[0] =~ /OPEN/) {
-		print "Air vent is open, closing it\n";
-		system("python close.py");
+
+# Make decisions
+if ($f < ($sp[0] - 0.5)) {
+	print "Half a degree (or more) below set point, too cold\n";
+	if ($mode[0] =~ /cool/) {
+		if ($ventstate[0] =~ /CLOSED/) {
+			print "Air vent already closed, nothing to do\n";
+		} elsif ($ventstate[0] =~ /OPEN/) {
+			print "Air vent is open, closing it\n";
+			system("python close.py");
+		}
+	} elsif ($mode[0] =~ /heat/) {
+		if ($ventstate[0] =~ /CLOSED/) {
+			print "Air vent is closed, opening it\n";
+			system("python open.py");
+		} elsif ($ventstate[0] =~ /OPEN/) {
+			if ($f > ($sp[0] - 1.5)) {
+				print "Air vent is already open, but it's 2 degrees (or more) below set point.  Starting furnace.\n";
+				system("python heat_on.py");
+			} else {
+				print "Air vent is already open, nothing to do\n";
+			}
+		}
 	}
 } elsif ($f > ($sp[0])) {
 	print "At or above set point\n";
-	if ($ventstate[0] =~ /OPEN/) {
-		if ($f > ($sp[0] + 1.75)) {
-			print "Air vent is already open, but it's >1.75F above set point.  Starting AC.\n";
-			system("python run.py > /dev/null 2>&1 &");
-		} else {
-			print "Air vent is already open, nothing to do\n";
+	if ($mode[0] =~ /cool/) {
+		if ($ventstate[0] =~ /OPEN/) {
+			if ($f > ($sp[0] + 1)) {
+				print "Air vent is already open, but it's 1 degree (or more) above set point.  Starting AC.\n";
+				system("python run.py > /dev/null 2>&1 &");
+			} else {
+				print "Air vent is already open, nothing to do\n";
+			}
+		} elsif ($ventstate[0] =~ /CLOSED/) {
+			print "Air vent is closed, opening it\n";
+			system("python open.py");
 		}
-	} elsif ($ventstate[0] =~ /CLOSED/) {
-		print "Air vent is closed, opening it\n";
-		system("python open.py");
+	} elsif ($mode[0] =~ /heat/) {
+		print "Stopping any furnace run jobs in progress\n";
+		system("python heat_off.py");
+		if ($f > ($sp[0] + 0.5)) {
+			print "Air vent is open, closing it\n";
+			system("python close.py");
+		}
 	}
 } else {
 	print "Temperature is okay, taking no action\n";
